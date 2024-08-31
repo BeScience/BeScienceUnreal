738a3db094f04cbeb2a0895957ab586218cb3b54
rlatkddn212 <rlatkddn212@naver.com>
2024-09-01 03:19:46 +0900

    차량 움직임 코드 추가

---
+++ Source/BeScienceUnreal/Private/Vehicle/BSU_VehicleWheelFront.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Vehicle/BSU_VehicleWheelFront.h"
+
+UBSU_VehicleWheelFront::UBSU_VehicleWheelFront()
+{
+	AxleType = EAxleType::Front;
+	bAffectedBySteering = true;
+	MaxSteerAngle = 40.f;
+
+	WheelRadius = 39.0f;
+	WheelWidth = 35.0f;
+	FrictionForceMultiplier = 3.0f;
+
+	MaxBrakeTorque = 4500.0f;
+	MaxHandBrakeTorque = 6000.0f;
+}
---
+++ Source/BeScienceUnreal/Public/Vehicle/BSU_VehicleWheelFront.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChaosVehicleWheel.h"
+#include "BSU_VehicleWheelFront.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API UBSU_VehicleWheelFront : public UChaosVehicleWheel
+{
+	GENERATED_BODY()
+
+public:
+
+	UBSU_VehicleWheelFront();
+};
