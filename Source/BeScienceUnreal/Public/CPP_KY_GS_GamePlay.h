8d53ba47441ab91dd7dccc0341ccf874d353f543
아기새 <liebespaar93@gmail.com>
2024-08-31 13:01:25 +0900

    make blueprint to cpp Core

---
+++ Source/BeScienceUnreal/Public/CPP_KY_GS_GamePlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_KY_GS_Core.h"
+#include "CPP_KY_GS_GamePlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API ACPP_KY_GS_GamePlay : public ACPP_KY_GS_Core
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BeScienceUnreal/Public/CPP_KY_WG_GameMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CPP_KY_WG_GameMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API UCPP_KY_WG_GameMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
