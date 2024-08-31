319472d5701fde430b9cdaf98235895d256177fa
아기새 <liebespaar93@gmail.com>
2024-08-31 15:03:02 +0900

    make blueprint to cpp Core (#8)

---
+++ Source/BeScienceUnreal/Public/CPP_KY_PC_GamePlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_KY_PC_Core.h"
+#include "CPP_KY_PC_GamePlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API ACPP_KY_PC_GamePlay : public ACPP_KY_PC_Core
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BeScienceUnreal/Public/CPP_KY_WG_BootScreen.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CPP_KY_WG_BootScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API UCPP_KY_WG_BootScreen : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	// 좀더 알아보고 wg 애니메이션 및 함수 호출 방법 알아내기
+	// wg 은 블루 프린트로
+};
