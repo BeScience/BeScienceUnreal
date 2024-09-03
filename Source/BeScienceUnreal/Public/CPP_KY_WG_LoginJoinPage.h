7f4babc1db03c34af4c19092a135c3ec6d367194
아기새 <liebespaar93@gmail.com>
2024-09-04 00:09:06 +0900

    add Network

---
+++ Source/BeScienceUnreal/Public/CPP_KY_WG_LoginJoinPage.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CPP_KY_WG_LoginPage.h"
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "CPP_KY_WG_LoginJoinPage.generated.h"
+
+/*
+ *
+ */
+UCLASS()
+class BESCIENCEUNREAL_API UCPP_KY_WG_LoginJoinPage : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UEditableTextBox* ET_Name;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UEditableTextBox* ET_Password;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UEditableTextBox* ET_PasswordCheck;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UButton* BT_Ok;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+		class UButton* BT_Cancel;
+
+	/*UFUNCTION(BlueprintImplementableEvent)
+	void MyNativeEvent();*/
+	UFUNCTION(BlueprintImplementableEvent)
+	void MyNativeEvent();
+
+public:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION()
+		void JoinButtonOnClick();
+
+	bool bHttpWaitresponse = false;
+
+	UFUNCTION()
+	void HttpPostJoinCall(FString username, FString password);
+
+	void OnResHttpPostJoinCallBack(FHttpRequestPtr req, FHttpResponsePtr res, bool bConnectedSuccessfully);
+
+	FString JsonStringfyJoinPlayer(FJsonPlayer player);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		FString URLLogin = "http://mtvs-museum.duckdns.org:8080/v1/auth/join";
+};
---
+++ Source/BeScienceUnreal/Public/CPP_KY_WG_LoginPage.h
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "CPP_KY_WG_LoginPage.generated.h"
+
+USTRUCT(BlueprintType)
+struct FJsonPlayer
+{
+	GENERATED_USTRUCT_BODY()
+
+		FJsonPlayer() : username("name"), password("UnKnown"){};
+
+	FJsonPlayer(FString name, FString password) : username(name), password(password){ };
+
+	UPROPERTY()
+		FString username;
+
+	UPROPERTY()
+		FString password;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API UCPP_KY_WG_LoginPage : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UEditableTextBox* ET_Name;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UEditableTextBox* ET_PassWord;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* BT_Ok;
+	
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* BT_Cancel;
+
+	virtual void NativeConstruct() override;
+
+	UFUNCTION()
+	void LoginButtonOnClick();
+
+	bool bHttpWaitresponse = false;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void MyNativeEvent();
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void HttpPostLoginCall(FString username, FString password);
+
+	void OnResHttpPostLoginCallBack(FHttpRequestPtr req, FHttpResponsePtr res, bool bConnectedSuccessfully);
+
+	UFUNCTION(BlueprintCallable)
+		FString UIJsonStringfyPlayer(FJsonPlayer player);
+
+	UFUNCTION(BlueprintCallable)
+		FJsonPlayer UIJsonPersePlayer(FString something);
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		FString URLLogin = "http://mtvs-museum.duckdns.org:8080/v1/auth/login";
+
+};
