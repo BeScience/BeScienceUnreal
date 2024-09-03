b32c08adcd78c64024177e2a49a9da67f20a8f81
아기새 <liebespaar93@gmail.com>
2024-09-04 00:09:40 +0900

    Kyoulee (#24)
    * need to fix ui interact
    
    * add Network

---
+++ Source/BeScienceUnreal/Public/CPP_KY_WG_LoginWelcomPage.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CPP_KY_WG_LoginWelcomPage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API UCPP_KY_WG_LoginWelcomPage : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BeScienceUnreal/Public/CPP_KY_WG_Login_DonePage.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CPP_KY_WG_Login_DonePage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESCIENCEUNREAL_API UCPP_KY_WG_Login_DonePage : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
